6c29cd2f4de938c9711fff567844b71df2698f00
khyunho <90512633+khyunho@users.noreply.github.com>
2023-07-07 14:09:49 +0900

    [level 2] Title: 할인 행사, Time: 0.06 ms, Memory: 68.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+        int count = 0;
+        
+        Map<String, Integer> discountMap = new HashMap<>();
+        Map<String, Integer> wantMap = new HashMap<>();
+        
+        for (int i=0; i<want.length; i++) {
+            wantMap.put(want[i], number[i]);
+        }
+        
+        for (int i=0; i<discount.length-9; i++) {
+            for (int j=i; j<i+10; j++) {
+                discountMap.put(discount[j], discountMap.getOrDefault(discount[j], 0) + 1);
+            }
+            
+            if (wantMap.equals(discountMap)) {
+                answer++;
+            }
+            
+            discountMap.clear();
+        }
+        
+        return answer;
+    }
+}

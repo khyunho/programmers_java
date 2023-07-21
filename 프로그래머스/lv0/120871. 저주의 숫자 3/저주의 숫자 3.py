d966a45ee9cdbdc733de397e310d43f28aa7fcb1
khyunho <90512633+khyunho@users.noreply.github.com>
2023-07-21 16:32:40 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.py
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.py
@@ -1,8 +1,7 @@
 def solution(n):
-    answer = 0
     i = 0
-    while answer < n:
+    for _ in range(n):
         i += 1
-        if i % 3 != 0 and "3" not in str(i):
-            answer += 1
+        while i % 3 == 0 or "3" in str(i):
+            i += 1
     return i

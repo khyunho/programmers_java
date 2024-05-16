eb43c50054b8c0c4fa687063f6098a4ac25cc584
khyunho <90512633+khyunho@users.noreply.github.com>
2024-05-16 15:33:27 +0900

    [level 2] Title: 하노이의 탑, Time: 2.51 ms, Memory: 103 MB -BaekjoonHub

---
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    private static List<int[]> arr = new ArrayList<>();
+
+    public int[][] solution(int n) {
+        move(n, 1, 2, 3);
+        return arr.toArray(int[][]::new);
+    }
+
+    private static void move(int cnt, int start, int mid, int end) {
+        if (cnt == 0) {
+            return;
+        }
+        move(cnt - 1, start, end, mid);
+        arr.add(new int[]{start, end});
+        move(cnt - 1, mid, start, end);
+    }
+}

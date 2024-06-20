7d706a997ddf62f44ae78dc1e97d08c68fd28478
khyunho <90512633+khyunho@users.noreply.github.com>
2024-06-20 15:47:57 +0900

    [level 1] Title: 덧칠하기, Time: 62.51 ms, Memory: 96.9 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/README.md
+++ 프로그래머스/1/161989. 덧칠하기/README.md
@@ -1,6 +1,6 @@
 # [level 1] 덧칠하기 - 161989 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161989#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161989) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 20일 15:47:24
+2024년 06월 20일 15:47:56
 
 ### 문제 설명
 
--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,29 +1,9 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n, int m, int[] section) {
-        if (m==1) {
-            return section.length;
-        }
-
-        int answer = 1;
-        int length = section[0]+m;
-        int distance = m-1;
-        List<Integer> list = new ArrayList<Integer>();
-
-        for (int value : section) {
-            list.add(value);
-        }
-
-        while (n >= length) {
-            if (list.contains(length)) {
-                answer++;
-                length += m;
-            } else {
-                length++;
-            }
-        }
-
+        int answer = 0;
+        
+        
+        
         return answer;
     }
 }

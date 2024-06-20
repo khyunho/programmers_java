3b96e31491df913dfeb7268ca2d276f3bd030412
khyunho <90512633+khyunho@users.noreply.github.com>
2024-06-20 14:07:56 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.04 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> solution(int n) {
+        List<Integer> answer = new ArrayList<>();
+        answer.add(n);
+        
+        while (n!=1) {
+            if (n%2==0) {
+                n = n/2;
+                answer.add(n);
+            } else {
+                n = n*3+1;
+                answer.add(n);
+            }
+        }
+        
+        return answer;
+    }
+}

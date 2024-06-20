95c9d5072561527e995a99edc42d63d9f58a1d97
khyunho <90512633+khyunho@users.noreply.github.com>
2024-06-20 16:25:31 +0900

    [level 1] Title: 덧칠하기, Time: 86.09 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -2,14 +2,38 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int m, int[] section) {
-        int roller = section[0];
-        int cnt = 1;
-        for(int i = 1; i < section.length; i++) {
-            if(roller + m - 1 < section[i]) {
-                cnt++;
-                roller = section[i];
+        if (m==1) {
+            return section.length;
+        }
+
+        int answer = 1;
+        int length = section[0]+m;
+        List<Integer> list = new ArrayList<Integer>();
+
+        for (int value : section) {
+            list.add(value);
+        }
+
+        while (n >= length) {
+            if (list.contains(length)) {
+                answer++;
+                length += m;
+            } else {
+                length++;
             }
         }
-        return cnt;
+        
+        // 속도 차이 심함
+        // int roller = section[0];
+        // int cnt = 1;
+        // for(int i = 1; i < section.length; i++) {
+        //     if(roller + m - 1 < section[i]) {
+        //         cnt++;
+        //         roller = section[i];
+        //     }
+        // }
+        // return cnt;
+
+        return answer;
     }
 }

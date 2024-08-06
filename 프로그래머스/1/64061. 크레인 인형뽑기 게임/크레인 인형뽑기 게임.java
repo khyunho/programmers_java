40f98f6a8914705fe557858ae195d51ae32be442
khyunho <90512633+khyunho@users.noreply.github.com>
2024-08-06 14:28:01 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.76 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        int[][] machine = new int[board.length][board[0].length];
+        List<Integer> basket = new ArrayList<>();
+
+        for (int i = 0; i < board.length; i++) {
+            int[] arr = new int [board[i].length];
+            for (int j = board.length - 1; j >= 0; j--) {
+                arr[j] = board[j][i];
+            }
+            machine[i] = arr;
+        }
+
+        for (int i = 0; i < moves.length; i++) {
+            for (int j = 0; j < machine[moves[i]-1].length; j++) {
+                if (machine[moves[i]-1][j] != 0) {
+                    if (basket.size() > 0 && basket.get(basket.size()-1) == machine[moves[i]-1][j]) {
+                        basket.remove(basket.size()-1);
+                        answer += 2;
+                        machine[moves[i]-1][j] = 0;
+                        break;
+                    } else {
+                        basket.add(machine[moves[i]-1][j]);
+                        machine[moves[i]-1][j] = 0;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+}
+
